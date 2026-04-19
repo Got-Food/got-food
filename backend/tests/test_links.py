e914088e41c295e360bd8ea2e997c4bdb20abcb9
Ryan Erickson <ryane25@vt.edu>
2026-04-19 18:24:20 -0400

    Updated test_links to check alive links more inclusively. Specifically checks that status codes aren't error codes, and if they are, that they're not in a predefined exception list.

--- backend/tests/test_links.py
+++ backend/tests/test_links.py
@@ -19,23 +19,25 @@ def test_all_links_valid(client):
 
 
 def test_all_links_alive(client):
-    # Get initial pantries from DB
-    response = client.get("/api/pantries")
-    assert response.status_code == 200
+    # Codes in client/server error range that do not indicate dead links
+    ERROR_CODE_EXCEPTIONS = {405, 406, 429, 503}
 
     # Ensure all pantry URLs are functional. Use browser-like headers to avoid
     # 403 blocks.
-    headers = {
+    HEADERS = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
         "Accept-Language": "en-US,en;q=0.9",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Connection": "keep-alive",
     }
+
+    # Get initial pantries from DB
+    response = client.get("/api/pantries")
+    assert response.status_code == 200
+
     data = response.get_json()
     for pantry in data:
-        try:
-            response = requests.get(pantry["url"], headers=headers)
-            assert response.status_code == 200 or response.status_code == 403
-        except AssertionError as e:
-            print(f"ERROR: URL {pantry["url"]} is dead, check manually; {e}")
-            raise e
+        response = requests.get(pantry["url"], headers=HEADERS)
+        assert (
+            response.status_code < 400 or response.status_code in ERROR_CODE_EXCEPTIONS
+        ), f"ERROR: URL {pantry["url"]} is dead, check manually."
