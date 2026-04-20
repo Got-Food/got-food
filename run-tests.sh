e0605db5bfb5ade8e85ae3f6a628141d142d5193
Ryan Erickson <ryane25@vt.edu>
2026-04-19 23:13:32 -0400

    Backend dockerfiles are ready for production. fixed main development docker-compose file. added dockerfile.dev in the frontend to separate npm run dev from the eventual prod command. created a test running script.

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-docker compose -f ./docker-compose.test.yml down -v
-docker compose -f ./docker-compose.test.yml up --build --abort-on-container-exit
+docker compose -f ./docker-compose.test.yml down -v && \
+docker compose -f ./docker-compose.test.yml build --no-cache && \
+docker compose -f ./docker-compose.test.yml up --abort-on-container-exit;
+
+# Always run docker compose down to kill any dangling containers
 docker compose -f ./docker-compose.test.yml down -v
