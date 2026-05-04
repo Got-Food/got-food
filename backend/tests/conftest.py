4923c25338076335e0a922086fa9bbaa88cf0088
Ryan Erickson <ryane25@vt.edu>
2026-05-04 01:43:50 -0400

    API load tests refactored to use Locust, which allows us to get more granular load test information and data reports.

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,5 +1,14 @@
+# Perform monkey patching before any imports or test config so Locust HTTP
+# management works correctly.
+from gevent import monkey
+
+monkey.patch_all()
+
 import pytest
+import requests
+import time
 from app import create_app, database as db
+from gevent.pywsgi import WSGIServer
 from unittest.mock import patch
 
 
@@ -11,6 +20,7 @@ def app():
             "TESTING": True,
         }
     )
+
     yield app
 
 
@@ -19,8 +29,40 @@ def client(app):
     return app.test_client()
 
 
+@pytest.fixture()
+def live_app():
+    """Launches a live instance of the application for use outside of the test
+    client context. Currently primarily used for the Locust load balancing tests.
+
+    Clients can connect to the application via http://localhost:5000. Functions
+    that need to call to the live instance should include this fixture in their
+    parameters, as this is a function-scoped fixture.
+    """
+    app = create_app()
+    server = WSGIServer(("127.0.0.1", 5000), app)
+    server.start()
+
+    # wait until server is actually ready
+    for _ in range(20):
+        try:
+            requests.get("http://localhost:5000")
+            break
+        except requests.ConnectionError:
+            time.sleep(0.1)
+
+    yield
+
+    # Clean up after test function exits
+    server.stop()
+
+
 def bind_commit_to_savepoint():
-    # flush to a nested savepoint instead of real commit
+    """Flush to a nested savepoint instead of a real commit.
+
+    This is set for our tests to prevent the pollution of the database by test
+    commits. It keeps the DB state clean between tests by creating a savepoint
+    that we can roll back to instead of a DB commit.
+    """
     db.session.begin_nested()
 
 
